ac65331ce86374f9575045a8a97d1e125fdc8dc2
shivamparwate <shivamparvate61@gmail.com>
2025-09-08 22:06:38 +0530

    some changes

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 // QUIZ DATA (Questions)
 const quizData = [
   {
-    question: "What does HTML stand for?",
-    options: [
-      "Hyper Text Markup Language",
-      "Home Tool Markup Language",
-      "High Tech Markup Language",
-      "Hyperlink and Text Making Language"
-    ],
-    answer: "Hyper Text Markup Language"
-  },
-  {
     question: "Which language runs in the browser?",
     options: ["Java", "C", "Python", "JavaScript"],
     answer: "JavaScript"
@@ -21,24 +11,9 @@ const quizData = [
     answer: "<link>"
   },
   {
-    question: "What does CSS stand for?",
-    options: [
-      "Computer Style Sheets",
-      "Cascading Style Sheets",
-      "Creative Style Sheets",
-      "Colorful Style Sheets"
-    ],
-    answer: "Cascading Style Sheets"
-  },
-  {
     question: "Which HTML tag is used to create a hyperlink?",
     options: ["<a>", "<p>", "<link>", "<h1>"],
     answer: "<a>"
-  },
-  {
-    question: "Which property is used in CSS to change text color?",
-    options: ["font-style", "text-color", "color", "background-color"],
-    answer: "color"
   },
   {
     question: "Inside which HTML element do we put JavaScript code?",
@@ -49,11 +24,6 @@ const quizData = [
     question: "Which symbol is used for comments in JavaScript?",
     options: ["//", "/* */", "<!-- -->", "#"],
     answer: "//"
-  },
-  {
-    question: "Which HTML attribute is used to display an image?",
-    options: ["src", "href", "alt", "title"],
-    answer: "src"
   },
   {
     question: "Which method is used to select an element by ID in JavaScript?",
@@ -76,11 +46,6 @@ const quizData = [
     answer: "Netscape"
   },
   {
-    question: "Which HTML element is used for the largest heading?",
-    options: ["<h1>", "<h6>", "<heading>", "<head>"],
-    answer: "<h1>"
-  },
-  {
     question: "Which keyword is used to declare a variable in JavaScript?",
     options: ["var", "let", "const", "All of the above"],
     answer: "All of the above"
@@ -93,14 +58,14 @@ const quizData = [
 ];
 
 // HTML ELEMENTS
-const questionBox = document.getElementById("questionBox");
-const optionsBox = document.getElementById("optionsBox");
-
-const scoreBox = document.getElementById("score");
-const highScoreBox = document.getElementById("highScore");
+const correctBox = document.getElementById("correct");
+const wrongBox = document.getElementById("wrong");
 const timerBox = document.getElementById("timer");
 
 const progressBar = document.getElementById("progressBar");
+
+const questionBox = document.getElementById("questionBox");
+const optionsBox = document.getElementById("optionsBox");
 
 const totalBox = document.getElementById("total");
 const qNumberBox = document.getElementById("qNumber");
@@ -110,9 +75,10 @@ const restartBtn = document.getElementById("restartBtn");
 
 // VARIABLES (Game State)
 let currentQuestion = 0;   // which question we are on
-let score = 0;             // player score
 let timer;                 // timer interval
 let timeLeft = 15;         // time per question
+let correct = 0;
+let wrong = 0;
 
 // show one question on the screen
 function showQuestion() {
@@ -121,19 +87,31 @@ function showQuestion() {
   timeLeft = 15;
   timerBox.innerText = timeLeft;
 
+  //get question from data
   const question = quizData[currentQuestion].question;
-  const answer = quizData[currentQuestion].answer;
 
   qNumberBox.innerText = currentQuestion + 1;
   totalBox.innerText = quizData.length;
   questionBox.innerText = question;
 
   // updating progress bar before updating next question
-
   const progressBarPercent = Math.floor((currentQuestion / quizData.length) * 100);
   progressBar.style.width = progressBarPercent + "%";
 
   // adding options for the questions
+  addOptions();
+
+  // disabled next btn till the user not select the answer
+  nextBtn.disabled = true;
+
+  // start timer
+  startTimer();
+}
+
+//add options one by one
+function addOptions() {
+  const answer = quizData[currentQuestion].answer;
+
   optionsBox.innerHTML = "";
   quizData[currentQuestion].options.forEach((opt) => {
     let btn = document.createElement("button");
@@ -141,14 +119,10 @@ function showQuestion() {
     btn.innerText = opt;
     optionsBox.appendChild(btn);
 
-    btn.addEventListener("click",()=>{
-      checkAnswer(btn,opt,answer);
+    btn.addEventListener("click", () => {
+      checkAnswer(btn, opt, answer);
     });
   });
-
-  nextBtn.disabled = true;
-
-  startTimer();
 }
 
 // check if selected answer is correct or wrong
@@ -159,10 +133,12 @@ function checkAnswer(selectedBtn, selectedBtnAnswer, correctAnswer) {
 
   if (selectedBtnAnswer === correctAnswer) {
     selectedBtn.classList.add("list-group-item-success");
-    score++;
-    scoreBox.innerText = score;
+    correct++;
+    correctBox.innerText = correct;
   } else {
     selectedBtn.classList.add("list-group-item-danger");
+    wrong++;
+    wrongBox.innerText = wrong;
     optionButtons.forEach(b => {
       if (b.innerText === correctAnswer) {
         b.classList.add("list-group-item-success");
@@ -217,20 +193,18 @@ nextBtn.addEventListener("click", () => {
 // End of quiz
 function endGame() {
   stopTimer();
-  questionBox.innerText = `🎉 Game Over! You scored ${score} / ${quizData.length}`;
+  questionBox.innerText = `🎉 Game Over! You scored ${correct} / ${quizData.length}`;
   optionsBox.innerHTML = "";
   nextBtn.style.display = "none";
   restartBtn.classList.remove("d-none");
   progressBar.style.width = "100%";
-  // update high score
-  highScoreBox.innerText = score;
 }
 
 // Restart Quiz
 restartBtn.addEventListener("click", () => {
   currentQuestion = 0;
-  score = 0;
-  scoreBox.innerText = score;
+  correctBox.innerText = 0;
+  wrongBox.innerText = 0;
   nextBtn.style.display = "inline-block";
   restartBtn.classList.add("d-none");
   showQuestion();
